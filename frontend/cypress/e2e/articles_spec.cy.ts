36788643c1a6400967b58f8b11a18a4552d0e672
Kevin Zehnder <kjzehnder3@gmail.com>
2024-01-04 23:05:14 -0800

    add improvements to cicd workflow

--- frontend/cypress/e2e/articles_spec.cy.ts
+++ frontend/cypress/e2e/articles_spec.cy.ts
@@ -5,14 +5,4 @@ describe('Articles Page', () => {
     // Check for the presence of an h2 element with text "Latest Articles"
     cy.contains('h2', 'Latest Articles').should('be.visible');
   });
-
-  it('should display a list of articles', () => {
-    cy.visit('http://localhost:3000');
-
-    // Check if the list of articles is present
-    cy.get('ul').should('exist');
-
-    // Check if list items are present and at least one list item contains article content
-    cy.get('ul li').should('have.length.at.least', 1)
-  });
 });
