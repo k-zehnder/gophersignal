2a64a0d03f82e27d944de6d4acc4fb2eaa162c99
Kevin Zehnder <kjzehnder3@gmail.com>
2024-01-10 02:07:17 -0800

    add `Latest Articles` section to frontend and update github actions (#99)
    * add `Latest Articles` section to frontend and update github actions
    
    * fix github action
    
    * eliminate the screenshot handling in github actions
    
    * more improvements to github actions workflow

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Description from '../components/Description';
 import ArticleList from '../components/ArticleList';
+import Typography from '@mui/joy/Typography';
 
-function Articles() {
+function Index() {
   return (
     <Layout>
       <Description />
+      <Typography
+        level="h2"
+        component="h2"
+        sx={{ fontWeight: 'bold', mb: 4, fontSize: '2rem' }}
+      >
+        Latest Articles
+      </Typography>
       <ArticleList />
     </Layout>
   );
 }
 
-export default Articles;
+export default Index;
