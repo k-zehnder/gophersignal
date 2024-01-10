62d78cadd50d88f9933651ab5b00823f7bf10ab2
Kevin Zehnder <kjzehnder3@gmail.com>
2024-01-09 19:26:32 -0800

    handle missing article summaries in ui

--- frontend/lib/siteMetaData.ts
+++ frontend/lib/siteMetaData.ts
@@ -1,7 +1,7 @@
 export const siteMetaData = {
   title: "Gopher Signal",
   author: "Mr. Ritchie",
-  description: "Dive into the world of technology with Gopher Signal, your go-to source for concise and insightful summaries of the latest and most impactful articles from HackerNews.com. Powered by the innovative capabilities of ChatGPT, Gopher Signal distills complex information into easy-to-digest updates, saving you time while keeping you informed. Join us for a unique blend of technology, insights, and discussions, all streamlined for the tech enthusiasts and professionals alike.",
+  description: "Dive into the world of technology with Gopher Signal, your go-to source for concise and insightful summaries of the latest and most impactful articles from HackerNews.com. Powered by the innovative capabilities of ChatGPT and HuggingFace, Gopher Signal distills complex information into easy-to-digest updates, saving you time while keeping you informed. Join us for a unique blend of technology, insights, and discussions, all streamlined for the tech enthusiasts and professionals alike.",
   language: "en-us",
   siteUrl: "https://gophersignal.com",
   image: "/images/photo.jpeg",
