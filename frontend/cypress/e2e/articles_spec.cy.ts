f686c875922649207110954f1606a0da607a7816
Kevin Zehnder <kjzehnder3@gmail.com>
2024-01-10 20:51:10 -0800

    use docker-compose in github actions

--- frontend/cypress/e2e/articles_spec.cy.ts
+++ frontend/cypress/e2e/articles_spec.cy.ts
@@ -2,7 +2,7 @@ export {};
 
 describe('Articles Page', () => {
   it('should display the title "Latest Articles"', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://0.0.0.0:3000');
 
     // Check for the presence of an h2 element with text "Latest Articles"
     cy.contains('h2', 'Latest Articles').should('be.visible');
