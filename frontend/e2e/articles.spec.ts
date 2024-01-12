29e58f46db0720a829fd5a4470cbab1d0f7b5ef7
Kevin Zehnder <kjzehnder3@gmail.com>
2024-01-11 17:37:34 -0800

    add Playwright test & simplify scraper

--- frontend/e2e/articles.spec.ts
+++ frontend/e2e/articles.spec.ts
@@ -1,18 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-
-  // Expect a title "to contain" a substring.
+  await page.goto('http://localhost:3000/');
   await expect(page).toHaveTitle(/Gopher Signal/);
-});
-
-test('latest articles heading', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-
-  // Check for the presence of an h2 element with the text "Latest Articles"
-  const latestArticlesHeading = await page.$('h2:has-text("Latest Articles")');
-
-  // Expect that the "Latest Articles" heading exists in the DOM
-  await expect(latestArticlesHeading).toBeTruthy();
 });
