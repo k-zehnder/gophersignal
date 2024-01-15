b044ab3b784ff93059226917d1b94195d0673ad9
k-zehnder <k-zehnder@github.com>
2024-01-15 09:59:19 +0000

    Revert "refactor Makefile, imports, and CI; fix DB auto-creation"
    This reverts commit a1b82e8263df28a682342488c4c92b55cefcdf91.

--- frontend/lib/siteMetaData.ts
+++ frontend/lib/siteMetaData.ts
@@ -4,8 +4,8 @@ export const siteMetaData = {
   description: "Dive into the world of technology with Gopher Signal, your go-to source for concise and insightful summaries of the latest and most impactful articles from HackerNews.com. Powered by the innovative capabilities of ChatGPT and HuggingFace, Gopher Signal distills complex information into easy-to-digest updates, saving you time while keeping you informed. Join us for a unique blend of technology, insights, and discussions, all streamlined for the tech enthusiasts and professionals alike.",
   language: "en-us",
   siteUrl: "https://gophersignal.com",
-  image: "/images/photo.jpg",
+  image: "/images/photo.jpeg",
   ogImage: "/images/og-card.jpg",
-  twImage: "/images/photo.jpg",
+  twImage: "/images/og-card.jpg",
   locale: "en-US",
 };
