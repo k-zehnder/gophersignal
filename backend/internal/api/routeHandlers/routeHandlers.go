ee29bb8f4823b1b181e41e3432ed38194cb68c54
Kevin Zehnder <kjzehnder3@gmail.com>
2024-01-18 22:22:54 -0800

    add `swagger` docs

--- backend/internal/api/routeHandlers/routeHandlers.go
+++ backend/internal/api/routeHandlers/routeHandlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/k-zehnder/gophersignal/backend/internal/models"
 	"github.com/k-zehnder/gophersignal/backend/internal/store"
 )
 
@@ -15,14 +16,23 @@ func NewHandler(store store.Store) *Handler {
 	return &Handler{Store: store}
 }
 
+// GetArticles godoc
+// @Summary Get articles
+// @Description Retrieve a list of articles from the store
+// @Tags articles
+// @Accept json
+// @Produce json
+// @Success 200 {array} models.Response{data=[]models.ArticleResponse}
+// @Failure 500 {object} models.Response{data=string}
+// @Router /articles [get]
 func (h *Handler) GetArticles(w http.ResponseWriter, r *http.Request) {
 	articles, err := h.Store.GetArticles()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		h.jsonResponse(w, models.Response{Code: http.StatusInternalServerError, Status: "error", Data: "Failed to retrieve articles from the store"}, http.StatusInternalServerError)
 		return
 	}
-	
-	h.jsonResponse(w, articles, http.StatusOK)
+
+	h.jsonResponse(w, models.Response{Code: http.StatusOK, Status: "success", Data: articles}, http.StatusOK)
 }
 
 func (h *Handler) jsonResponse(w http.ResponseWriter, data interface{}, statusCode int) {
