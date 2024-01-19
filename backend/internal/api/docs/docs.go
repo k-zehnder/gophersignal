cf90e1b7b150c89f2b8aafa159441c5a47c6e168
Kevin Zehnder <kjzehnder3@gmail.com>
2024-01-18 23:01:45 -0800

    fix `swagger` docs url

--- backend/internal/api/docs/docs.go
+++ backend/internal/api/docs/docs.go
@@ -117,7 +117,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1",
-	Host:             "https://gophersignal.com",
+	Host:             "gophersignal.com",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "GopherSignal API",
