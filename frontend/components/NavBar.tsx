b6b021943c88430fb5fcc03af7c1a930b8757d2f
Kevin Zehnder <kjzehnder3@gmail.com>
2024-03-27 13:29:47 -0700

    add temp alert for `HuggingFace` API downtime

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -5,12 +5,6 @@ import Typography from "@mui/joy/Typography";
 import ModeButton from "./ModeButton";
 import { siteMetaData } from "../lib/siteMetaData";
 
-// Define the API URL based on the environment.
-const apiUrl =
-  process.env.NEXT_PUBLIC_ENV === "development"
-    ? "http://localhost:8080/swagger/index.html#/"
-    : "https://gophersignal.com/swagger/index.html#/";
-
 // Define navigation links for the NavBar.
 const navLinks = [
   {
@@ -21,16 +15,16 @@ const navLinks = [
     name: "About",
     path: "/about",
   },
-  {
-    name: "API",
-    path: apiUrl,
-  },
 ];
 
 // NavBar component for rendering the navigation bar.
 export default function NavBar() {
   return (
     <>
+      {/* Render the red alert bar to notify users that the API is down */}
+      <div style={{ backgroundColor: "red", color: "white", padding: "10px", textAlign: "center" }}>
+        The HuggingFace API is currently down and summaries are unavailable. We apologize for the inconvenience.
+      </div>
       <Link href="/">
         {/* Render the site title as a link to the home page */}
         <Typography component="h1" level="display2" fontSize="xl">
@@ -44,28 +38,14 @@ export default function NavBar() {
             flexDirection: { xs: "row", md: "column" },
           }}
         >
-          {navLinks.map(({ path, name }) => {
-            // Check if the path is an external URL or a local route.
-            if (path.startsWith("http")) {
-              return (
-                // Render an external link for URLs starting with "http".
-                <ListItem key={path}>
-                  <a href={path} target="_blank" rel="noopener noreferrer">
-                    {name}
-                  </a>
-                </ListItem>
-              );
-            } else {
-              return (
-                // Render a link to a local route for other paths.
-                <ListItem key={path}>
-                  <Link href={path}>{name}</Link>
-                </ListItem>
-              );
-            }
-          })}
+          {navLinks.map(({ path, name }) => (
+            // Render a link to a local route for each navigation item
+            <ListItem key={path}>
+              <Link href={path}>{name}</Link>
+            </ListItem>
+          ))}
           <ListItem>
-            {/* Render the ModeButton component for light/dark mode toggle. */}
+            {/* Render the ModeButton component for light/dark mode toggle */}
             <ModeButton />
           </ListItem>
         </List>
