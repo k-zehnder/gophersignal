4c8e155824f720a13f1e406239f0ea91d66ea9f9
Kevin Zehnder <kjzehnder3@gmail.com>
2024-04-25 19:26:45 -0700

    `refactor`: clean up next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,11 +14,6 @@ const nextConfig = {
     
     // distDir: 'dist',
 
-    compiler: {
-      // Removes any disabling of SWC if previously set
-      styledComponents: true,
-    },
-
     swcMinify: true, // Enables SWC minification
   }
 
